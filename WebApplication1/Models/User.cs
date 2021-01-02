d579babec8cae7609b8d5637803d24e1afa2c28c
Judea Dumont <judea.dumont@viavisolutions.com>
2021-01-02 05:17:01 -0700

    User Model

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Models
+{
+    public class User
+    {
+        public User(string sUser, int sId, string sPassword)
+        {
+            ID = sId;
+            Name = sUser;
+            Password = sPassword;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
